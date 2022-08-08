8fdbff2c0dcf1b9fbeeae2d8e9c939fd542b28e9
Sam Macharia <smm_mwangii@live.com>
2022-08-08 21:50:29 +0100

    watcher

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,55 @@
 <script setup>
+import { ref, watch } from "vue";
+
+const count = ref(0);
+const joke = ref("");
+
+watch(count, async () => {
+  const res = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  joke.value = (await res.json()).joke;
+});
+
+function requestJoke() {
+  count.value++;
+}
 </script>
 
 <template>
   <main>
-    <h1>This is an awesome place to be</h1>
+    <h1>Watchers</h1>
+    <p>Click the burron to request a dad joke</p>
+    <button @click="requestJoke">Request Joke</button>
+
+    <blockquote>
+      <p>{{ joke }}</p>
+    </blockquote>
   </main>
 </template>
+
+<style scoped>
+p {
+  margin: 20px 0;
+}
+blockquote {
+  font-size: 2em;
+}
+
+button {
+  border: none;
+  margin: 0;
+  padding: 1.2em;
+  width: auto;
+  overflow: visible;
+  border-radius: 3px;
+
+  background: white;
+}
+
+button:hover {
+  cursor: pointer;
+}
+</style>
