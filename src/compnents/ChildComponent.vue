a9fec99625e91880b701dc25ece15c2896c966af
Sam Macharia <smm_mwangii@live.com>
2022-08-08 22:30:29 +0100

    Prop stuff

---
+++ src/compnents/ChildComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { computed } from "vue";
+
+defineProps({ dataPassed: String });
+
+// const props = defineProps(["initialValue"]);
+
+// const transformedValue = computed(() => props.initialValue * 2);
+</script>
+
+<template>
+  <h2>I am a child</h2>
+  <h3>{{ dataPassed }}</h3>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
-<script setup></script>
+<script setup>
+import ChildComponent from "@/compnents/ChildComponent.vue";
+</script>
 
 <template>
   <main>
-    <h1>Welcome to our Vue 3 Essentials Intro</h1>
-    <p>Sit down and enjoy the ride</p>
-    <p>I'll be your cpatian today so look at me</p>
+    <h1>Props</h1>
+    <p>This is a parent component</p>
+    <br />
+    <ChildComponent dataPassed="This is data passed from the parent" />
   </main>
 </template>
 
