2b10dde0b747b478211692461966c14c2420e6fe
Sam Macharia <smm_mwangii@live.com>
2022-08-08 22:42:47 +0100

    Events in Vue 3

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+const emit = defineEmits(["notify"]);
+
+function buttonClick() {
+  emit("notify");
+}
+</script>
+
+<template>
+  <main>
+    <button @click="buttonClick">Notify Parent</button>
+  </main>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ChildComponent from "@/components/ChildComponent.vue";
+
+const notificationMessage = ref("");
+
+function notify() {
+  notificationMessage.value = "You have been notified my good man";
+}
+</script>
 
 <template>
   <main>
-    <h1>Welcome to our Vue 3 Essentials Intro</h1>
-    <p>Sit down and enjoy the ride</p>
-    <p>I'll be your cpatian today so look at me</p>
+    <h1>Events</h1>
+    <br />
+
+    <h2>{{ notificationMessage }}</h2>
+
+    <ChildComponent @notify="notify" />
   </main>
 </template>
 
