c7fe0f34d58d5a94837e77a82be643dd7c53349b
Sam Macharia <smm_mwangii@live.com>
2022-08-08 23:04:54 +0100

    Typo update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
   <main>
     <h1>Welcome to our Vue 3 Essentials Intro</h1>
     <p>Sit down and enjoy the ride</p>
-    <p>I'll be your cpatian today so look at me</p>
+    <p>I'll be your captain today so look at me</p>
   </main>
 </template>
 
