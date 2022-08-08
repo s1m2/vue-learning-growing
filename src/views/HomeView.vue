ddb6dea82ff2b4349258e92e6d02e5bd98f1093e
Sam Macharia <smm_mwangii@live.com>
2022-08-08 18:16:19 +0100

    Implementing reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { reactive } from "vue";
+
+const state = reactive({ count: 0 });
+
+function incrementCount() {
+  state.count++;
+}
 </script>
 
 <template>
   <main>
-    <h1>This is an awesome place to be</h1>
+    <h1>The current count is: {{ state.count }}</h1>
+    <button @click="incrementCount">Increment Count</button>
   </main>
 </template>
